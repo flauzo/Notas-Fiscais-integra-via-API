5768969e6ec41e9cb73bea602efc9e5f0558a3bd
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-13 01:18:38 -0300

    Update NotaFiscalService.java

--- src/main/java/alpe/bruno/notafiscalapi/service/NotaFiscalService.java
+++ src/main/java/alpe/bruno/notafiscalapi/service/NotaFiscalService.java
@@ -1,14 +1,14 @@
-package alpe.bruno.notafiscalapi.service;
+package flauzo.cruz.notafiscalapi.service;
 
-import alpe.bruno.notafiscalapi.domain.dto.NotaFiscalDTO;
-import alpe.bruno.notafiscalapi.domain.enums.FormaPagamento;
+import flauzo.cruz.notafiscalapi.domain.dto.NotaFiscalDTO;
+import flauzo.cruz.notafiscalapi.domain.enums.FormaPagamento;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 /**
- * @author brunoabneves
+ * @author flauzo
  */
 public interface NotaFiscalService {
     NotaFiscalDTO processarNotaFiscal(UUID uuidCliente, MultipartFile xmlNotaFiscal,

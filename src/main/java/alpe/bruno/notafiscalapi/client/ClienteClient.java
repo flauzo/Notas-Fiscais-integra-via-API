a273f36405ed8c8b373fa1ad3894178a260d207d
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-13 04:56:33 -0300

    Update ClienteClient.java

--- src/main/java/alpe/bruno/notafiscalapi/client/ClienteClient.java
+++ src/main/java/alpe/bruno/notafiscalapi/client/ClienteClient.java
@@ -1,6 +1,6 @@
-package alpe.bruno.notafiscalapi.client;
+package flauzo.cruz.notafiscalapi.client;
 
-import alpe.bruno.notafiscalapi.client.response.ArquivosClienteResponse;
+import flauzo.cruz.notafiscalapi.client.response.ArquivosClienteResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.UUID;
 
 /**
- * @author brunoabneves
+ * @author flauzo
  */
 @FeignClient(name = "cliente-client", url = "https://www.url.ficticia")
 public interface ClienteClient {

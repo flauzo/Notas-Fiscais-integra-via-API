98db39ae68c5d9fb960fa05fed8485d0cf61881e
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-13 04:58:07 -0300

    Update ArquivosClienteResponse.java

--- src/main/java/alpe/bruno/notafiscalapi/client/response/ArquivosClienteResponse.java
+++ src/main/java/alpe/bruno/notafiscalapi/client/response/ArquivosClienteResponse.java
@@ -1,13 +1,13 @@
-package alpe.bruno.notafiscalapi.client.response;
+package flauzo.cruz.notafiscalapi.client.response;
 
-import alpe.bruno.notafiscalapi.domain.enums.FormaPagamento;
+import flauzo.cruz.notafiscalapi.domain.enums.FormaPagamento;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
 
 /**
- * @author brunoabneves
+ * @author flauzo
  */
 public record ArquivosClienteResponse(
         MultipartFile xmlNotaFiscal,

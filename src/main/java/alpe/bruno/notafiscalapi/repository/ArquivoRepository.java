2d1d12f41fd3aa2c21236a6a43f81c4c66b25185
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-13 03:40:26 -0300

    Update ArquivoRepository.java

--- src/main/java/alpe/bruno/notafiscalapi/repository/ArquivoRepository.java
+++ src/main/java/alpe/bruno/notafiscalapi/repository/ArquivoRepository.java
@@ -1,12 +1,12 @@
-package alpe.bruno.notafiscalapi.repository;
+package flauzo.cruz.notafiscalapi.repository;
 
-import alpe.bruno.notafiscalapi.domain.model.Arquivo;
+import flauzo.cruz.notafiscalapi.domain.model.Arquivo;
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * @author brunoabneves
+ * @author flauzo
  */
 @Repository
 @Hidden

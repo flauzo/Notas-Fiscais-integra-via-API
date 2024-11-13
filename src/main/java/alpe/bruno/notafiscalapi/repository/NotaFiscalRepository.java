ceca880cfb4ba5529b63bdeabc629f48a6a3d8a0
Cruz, Flausimar <88656939+flauzo@users.noreply.github.com>
2024-11-13 03:35:53 -0300

    Update NotaFiscalRepository.java

--- src/main/java/alpe/bruno/notafiscalapi/repository/NotaFiscalRepository.java
+++ src/main/java/alpe/bruno/notafiscalapi/repository/NotaFiscalRepository.java
@@ -1,11 +1,11 @@
-package alpe.bruno.notafiscalapi.repository;
+package flauzo.cruz.notafiscalapi.repository;
 
-import alpe.bruno.notafiscalapi.domain.model.NotaFiscal;
+import flauzo.cruz.notafiscalapi.domain.model.NotaFiscal;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * @author brunoabneves
+ * @author flauzo
  */
 @Repository
 public interface NotaFiscalRepository extends JpaRepository<NotaFiscal, Long> {
